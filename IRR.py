5837d87d51296e6695a2e574c306ceb09b68892a
Adolfo Aparicio <financieras@gmail.com>
2018-06-02 22:19:48 +0200

    Update IRR.py

--- IRR.py
+++ IRR.py
@@ -1,5 +1,5 @@
 def npv_f(rate, cashflows):
     total = 0.0
-    for i, cashflow in enumerate(cashflow):
+    for i, cashflow in enumerate(cashflows):
         total += cashflow / (1 +rate)**i
     return total
